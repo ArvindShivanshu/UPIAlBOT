0760576dc43d8fbe9d8e021c09097154d708a53a
Shivanshu Gupta <88023362+ArvindShivanshu@users.noreply.github.com>
2024-05-05 13:10:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,13 +26,13 @@ db = mongo_client["dem-ref-bot"]
 admin_chat_id = 5122882808
 
 min_with = 20
-required_channels = ["-1001856598536", "@GOAGAMESSURESHOTS", "-1001914613911", "@joinxyza", "-1002023923365"]
+required_channels = ["-1001856598536", "@GOAGAMESSURESHOTS", "-1001914613911", "@OTPAIBOTSUPPORT", "-1002023923365"]
 payment_channel = '@joinxyza'
 
 
 
 def is_chat_member(user_id):
-    channel_ids = ['-1001856598536', '@GOAGAMESSURESHOTS', '-1001914613911', '@joinxyza', '-1002023923365']
+    channel_ids = ['-1001856598536', '@GOAGAMESSURESHOTS', '-1001914613911', '@OTPAIBOTSUPPORT', '-1002023923365']
     user_id = str(user_id)  # Convert to string in case it's an integer
 
     for channel_id in channel_ids:
@@ -322,8 +322,8 @@ def send_join_message(message):
             types.InlineKeyboardButton(text="Join", url="https://t.me/+g968r6GDQm9jMjg1")
         )
         join_markup.row(
-            types.InlineKeyboardButton(text="Join", url="https://t.me/joinxyza"),
-            types.InlineKeyboardButton(text="Join", url="https://t.me/+cg5Kz8r3ZLUzMWQ9")
+            types.InlineKeyboardButton(text="Join", url="https://t.me/+cg5Kz8r3ZLUzMWQ9"),
+            types.InlineKeyboardButton(text="Join", url="https://t.me/OTPAIBOTSUPPORT")
         )
         
         join_markup.row(types.InlineKeyboardButton(text="Verify", callback_data="verify"))
